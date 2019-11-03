fefd755e42df44ec63b84eae2b7fbb599f535549
Kelly Small <xcxkelly98@gmail.com>
2019-11-03 22:56:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
 def create_an_empty_array
-  
+  Disney_movies = []
 end
 
 def create_an_array
-  
+  Disney_movies = [ "Frozen", "Brave", "Tangled" ]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  Disney_movies.push("Brother Bear")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  Disney_movies.unshift("Moana")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  "Brother Bear" = Disney_movies.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  "Moana" = Disney_movies.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  Disney_movies[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  Disney_movies.index("Frozen")
 end
 
 def retrieve_last_element_from_array(array)
-  
+  Disney_movies.index("Tangled")
 end
 
 def update_element_from_index(array, index_number, element)
-
+  Disney_movies[2] = "Wreck it Ralph"
+    =>"Wreck it Ralph"
 end
